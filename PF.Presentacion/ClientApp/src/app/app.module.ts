2dcf921b8ce3b109218b43419c8911ac3ab3f244
NotebookSalteño <danykapoc@gmail.com>
2019-10-21 17:09:56 -0300

    computo

--- PF.Presentacion/ClientApp/src/app/app.module.ts
+++ PF.Presentacion/ClientApp/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FamilyComponent } from './family/family/family.component';
 
 //Third party imports
 import { NgxLoadingModule } from "ngx-loading";
+import { CalculationComponent } from './calculation/calculation.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { NgxLoadingModule } from "ngx-loading";
     HomeComponent,
     CounterComponent,
     FetchDataComponent,
-    FamilyComponent
+    FamilyComponent,
+    CalculationComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
